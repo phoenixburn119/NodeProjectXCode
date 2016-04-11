83e885ccc13ff39768b2860942174ee8c23dbbcb
Adam Kinzer <adamkinzer915@gmail.com>
2016-04-11 11:59:22 -0600

    Added TreeNode

---
+++ Model/TreeNode.hpp
@@ -0,0 +1,14 @@
+//
+//  TreeNode.hpp
+//  NodeProject
+//
+//  Created by Kinzer, Adam on 4/11/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef TreeNode_hpp
+#define TreeNode_hpp
+
+#include "Node.h"
+
+#endif /* TreeNode_hpp */
