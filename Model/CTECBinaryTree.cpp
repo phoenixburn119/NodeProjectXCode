750e23bc3a6d3387d093c766847232c31061a537
Adam Kinzer <adamkinzer915@gmail.com>
2016-04-13 12:52:25 -0600

    Added contains method

--- Model/CTECBinaryTree.cpp
+++ Model/CTECBinaryTree.cpp
@@ -7,3 +7,64 @@
 //
 
 #include "CTECBinaryTree.hpp"
+using namespace CTECData;
+using namespace std;
+
+template <class Type>
+void CTECBinaryTree<Type> :: preorderTraversal(TreeNode<Type> * current)
+{
+  if(currentNode != nullptr)
+  {
+      cout << currentNode->getValue() << " ";
+      preorderTraversal(currentNode->getLeftChild());
+      preorderTraversal(currentNode->getRightChild());
+  }
+}
+
+template <class Type>
+void CTECBinaryTree<Type> :: inorderTraversal(Tree<Type> * currentNode)
+{
+    if(currentNode != nullptr)
+    {
+        preorderTraversal(currentNode->getLeftChild());
+        cout << currentNode->getValue() << " ";
+        preorderTraversal(currentNOde->getRightChild());
+    }
+}
+
+template <class Type>
+void CTECBinaryTree<Type> :: postorderTraversal(Tree<Type> * currentNode)
+{
+    if(currentNode != nullptr)
+    {
+        preorderTraversal(currentNode->getLeftChild());
+        preorderTraversal(currentNOde->getRightChild());
+        cout << currentNode->getValue() << " ";    }
+}
+
+template <class Type>
+bool CTECBinaryTree<Type> :: contains(Type value)
+{
+    bool isInTree = false;
+    
+    if(root != nullptr)
+    {
+        if(root->getValue == value)
+        {
+            isInTree = true;
+        }
+        else
+        {
+            if(value < root->getValue())
+            {
+                isInTree = contains(value, root->getLeftChild());
+            }
+            else
+            {
+                isInTree = contains(value, root->getRigChild());
+            }
+        }
+    }
+    
+    return isInTree;
+}
