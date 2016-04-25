e60a4943568bd31209287a2be0b41f17aa9ab9f1
Adam Kinzer <adamkinzer915@gmail.com>
2016-04-25 12:20:24 -0600

    Added to CTEGGraph.cpp.

--- Model/CTECGraph.cpp
+++ Model/CTECGraph.cpp
@@ -7,3 +7,30 @@
 //
 
 #include "CTECGraph.hpp"
+
+using namespace CTECData;
+
+template <class Type>
+const int CTECGraph<Type> :: MAXIMUM;
+
+template <class Type>
+void CTECGraph<Type> :: addVertex(const Type& value)
+{
+    assert(size() < MAXIMUM);
+    int newVertexNumber = manyVertices;
+    manyVertices++;
+    
+    for(int otherVertexNumber = 0; otherVertexNumber < manyVertices; otherVertexNumber++)
+        {
+            adjacencyMatrix[otherVertexNumber][newVertexNumber] = false;
+            adjacencyMatrix[newVertexNumber][otherVertexNumber] = false;
+        }
+    labels[newVertexNumber] = value;
+}
+
+template <class Type>
+void CTECGraph<Type> :: addEdge(int source, int target)
+{
+    assert(source < size() && target < size());
+    adjacencyMatrix[source][target] = true;
+}
