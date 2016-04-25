cbc7ce8176ac776203b100d6404202715e258c21
Adam Kinzer <adamkinzer915@gmail.com>
2016-04-25 12:36:20 -0600

    Added more to the CTECGraph.cpp

--- Model/CTECGraph.cpp
+++ Model/CTECGraph.cpp
@@ -34,3 +34,55 @@ void CTECGraph<Type> :: addEdge(int source, int target)
     assert(source < size() && target < size());
     adjacencyMatrix[source][target] = true;
 }
+
+template <class Type>
+bool CTECGraph<Type> :: isEdge(int source, int target) const
+{
+    assert(source < size() && target < size());
+    
+    bool isAnEdge = false;
+    isAnEdge = adjacencyMatrix[source][target];
+    
+    return isAnEdge;
+}
+
+template <class Type>
+Type& CTECGraph<Type> :: operator[](int vertex)
+{
+    assert(vertex < size());
+    return labels[vertex];
+}
+
+template <class Type>
+Type CTECGraph<Type> :: operator[](int vertex) const
+{
+    assert(vertex < size());
+    return labels[vertex];
+}
+
+template <class Type>
+std::set<int> CTECGraph<Type> :: neighbors(int vertex) const
+{
+    assert(vertex < size());
+    std::set<int> vertexNeighbors;
+    
+    for(int index = 0; index < size(); index++)
+    {
+        if(adjacencyMatrix[vertex][index])
+        {
+            vertexNeighbors.insert(index);
+        }
+    }
+    return vertexNeighbors;
+}
+
+template <class Type>
+void CTECGraph<Type> :: removeEdge(int source, int target)
+{
+    assert(source < size() && target < size());
+    adjacencyMatrix[source][target] = false];
+    
+    
+    
+    
+}
